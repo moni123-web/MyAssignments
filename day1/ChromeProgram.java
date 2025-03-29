a3bd9489351b021700b570a6b47182df16865949
moni123-web <monisharimi98@gmail.com>
2025-03-29 23:30:00 +0530

    Add files via upload
    day1

---
+++ day1/ChromeProgram.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class ChromeProgram {
+
+	public static void main(String[] args) {
+		//float
+		float rateOfInterest=91.0f;
+		System.out.println(rateOfInterest);
+		//string
+		String name="Google";
+		System.out.println(name);
+		//boolean
+		boolean no=false;
+		System.out.println(no);
+		//short
+		short releaseOfYear=2008;
+		System.out.println(releaseOfYear);
+		//char
+		char logo='c';
+		System.out.println(logo);
+		
+		
+		
+		
+
+	}
+
+}
